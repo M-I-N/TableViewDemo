72b420d3466c0318d5019363b794335b0632b8f3
Nayem BJIT <nayem@mac-mini.local>
2017-06-20 16:49:52 +0600

    First Working Copy

---
+++ TVtoTV/FirstTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  FirstTableViewController.swift
+//  TVtoTV
+//
+//  Created by Nayem BJIT on 6/20/17.
+//  Copyright © 2017 Nayem BJIT. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = "Tap Me"
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextScene = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondTableViewController
+        nextScene.rowCount = 5
+        self.navigationController?.pushViewController(nextScene, animated: true)
+    }
+
+}
---
+++ TVtoTV/SecondTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SecondTableViewController.swift
+//  TVtoTV
+//
+//  Created by Nayem BJIT on 6/20/17.
+//  Copyright © 2017 Nayem BJIT. All rights reserved.
+//
+
+import UIKit
+
+class SecondTableViewController: UITableViewController {
+    
+    var rowCount = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return rowCount
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "anotherCell", for: indexPath)
+
+        cell.textLabel?.text = "Hey There"
+
+        return cell
+    }
+
+
+}
